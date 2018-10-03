4443cd1d085ea987a7e4ac43ee46c6ed15f00205
jankilighthouse <jankipatel6890@gmail.com>
2018-10-03 21:31:02 +0000

    successfully sending message to server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -3,7 +3,6 @@ const SocketServer = require('ws').Server;
 
 // Set the port to 3001
 const PORT = 3001;
-
 // Create a new express server
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
@@ -12,13 +11,46 @@ const server = express()
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
+let sharedMessage = '';
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(data));
+      console.log('data sent to client from server');
+
+    })
+   }
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
+// Broadcast to all.
+
+
+    ws.on('message',(clientData) => {
+    const data = JSON.parse(clientData);
+    console.log('HELLOOOOOOOO',data)
+
+      switch(data.type){
+        case 'postmessage':
+        data.type = 'incMessage';
+        sharedMessage=data;
+        wss.broadcast(sharedMessage);
+        break;
+
+        case 'postUser':
+        data.type = 'incUser';
+        sharedMessage=data;
+        wss.broadcast(sharedMessage);
+        break;
+
+        default:
+        throw new Error('Unknown event type ' + data.type);
+      }
+    })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
+
+
